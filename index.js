3eedf5b93c2ea5bdb1c305867334d25a9a13e2b5
terciobrasil <terciobrasil21@hotmail.com>
2024-02-11 19:48:40 -0300

    Desafio do FELIPAO

---
+++ index.js
@@ -0,0 +1,81 @@
+var NomePersonagem = "Cisne Negro"
+var XP = 10010
+
+if(XP < 1000){
+        nivel = "ferro"
+        //console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+        //console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+        //console.log("Seu nivel é " + nivel)
+        
+        //Se XP for menor do que 1.000 = Ferro                
+
+}
+else if(XP < 2000){
+        nivel = "Bronze"    
+        //console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+        //console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+        //console.log("Seu nivel é " + nivel)
+        //Se XP for entre 1.001 e 2.000 = Bronze 
+}
+else if(XP <= 5000){
+        nivel = "Prata"    
+        //console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+        //console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+        //console.log("Seu nivel é " + nivel)
+        //Se XP for entre 2.001 e 5.000 = Prata   
+}
+else if(XP <= 7000){
+        nivel = "Ouro"    
+        //console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+        //console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+        //console.log("Seu nivel é " + nivel)
+        //Se XP for en tre 5.001 e 7.000 = Ouro    
+         
+}
+else if(XP <= 8000){
+        nivel = "Platina"    
+        //console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+        //console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+        //console.log("Seu nivel é " + nivel)
+        //Se XP for entre 7.001 e 8.000 = Platina         
+}
+else if(XP  <= 9000){
+        nivel = "Ascendente"    
+        //console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+        //console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+        //console.log("Seu nivel é " + nivel)
+        //Se XP for entre 8.001 e 9.000 = Ascendente
+      
+}
+else if(XP <= 10000 ){
+        nivel = "Imortal"    
+        //console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+        //console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+        //console.log("Seu nivel é " + nivel)
+        //Se XP for entre 9.001 e 10.000= Imortal
+        
+}
+else{
+        nivel = "Radiante"    
+        //console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+        //console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+        //console.log("Seu nivel é " + nivel)
+        //Se XP for maior ou igual a 10.001 = Radiante
+}      
+console.log("É esse é o nome do seu Personagem! " + NomePersonagem)
+console.log("Você tem a quantidfade de "+ XP +" XPs na sua conta.")
+console.log("Seu nivel é " + nivel)
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+
+
+    
