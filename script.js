38e70e11b88ef0106714ac20003332484d24498b
terciobrasil <terciobrasil21@hotmail.com>
2024-02-13 20:31:46 -0300

    teste de função

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 
-let fruta =["Banana", "Orange", "Apple", "Mango"]
-document.getElementById('demo').innerHTML=fruta.toString()
+//let fruta =["Banana", "Orange", "Apple", "Mango"]
+//document.getElementById('demo').innerHTML=fruta.toString()
+
+function myFunção(tipoArroz, tipofeijao,tipoCarne){
+    var peso = 1000
+    var tara = 120
+    var pesoTotal = peso - tara
+    var valorPeso = 61.9
+    var pesoFinal = pesoTotal*valorPeso/1000
+    
+    
+    console.log("Seu prato esta com "+ tipoArroz + ", " +tipofeijao+ " e "+tipoCarne)
+    console.log("Valor do peso kg R$: " +pesoFinal)
+}
+
+//myFunção("Maria Isabel","Feijão Preto","Carne Assada")
+myFunção("Arroz Branco","Feijão Carioca","Frango na Brasa")
